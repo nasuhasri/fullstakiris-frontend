a0618a645178b9b063811f04b53fd2db83d3e505
Nasuha Asri <nasuhasri00@gmail.com>
2022-09-07 23:33:14 +0800

    add input form

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,39 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-app-bar app color="primary" dark>
+      <div class="d-flex align-center">
+        <span class="mr-2">My Cool Model</span>
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn href="https://github.com/nasuhasri/irisapp" target="_blank" text>
+        <span class="mr-2">Fork Me</span>
+        <v-icon>mdi-open-in-new</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <!-- <HelloWorld /> -->
+      <HomePage />
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from "./components/HelloWorld";
+import HomePage from "./components/HomePage.vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+  name: "App",
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  components: {
+    // HelloWorld,
+    HomePage,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
